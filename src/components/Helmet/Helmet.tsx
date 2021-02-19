72f29ebc2ec3e51d393f4817ba3233b3db045e5d
Leng <lengzhang@live.com>
2021-02-18 19:34:28 -0800

    updated app page title

---
+++ src/components/Helmet/Helmet.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const AppHelmet: React.FC = () => {
+  return (
+    <Helmet>
+      <title>2048 by Leng</title>
+    </Helmet>
+  );
+};
+
+export default AppHelmet;
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import * as React from "react";
 import { render } from "react-dom";
 
 import Game from "./components/Game";
+import Helmet from "./components/Helmet";
 
 function App() {
-  return <Game />;
+  return (
+    <>
+      <Helmet />
+      <Game />
+    </>
+  );
 }
 
 const rootElement = document.getElementById("root");
